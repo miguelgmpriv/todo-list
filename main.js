6e3df658b27e42d4f5cb8bb033a3fbbc23914fd2
Miguel Gonzalez <miguelgm.priv@gmail.com>
2021-10-09 18:14:21 -0700

    Add initial project files wepack.config.js created css, style loader, html plugin and dev server installed console.log message displayed succesfully

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG9CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIZWxsbycpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
