bdfea8bd3cb997fbe7930126c120a8c0266e9b89
Miguel Gonzalez <miguelgm.priv@gmail.com>
2021-10-10 14:33:02 -0700

    Add html for modal form Using html template tags Added javascript to show modal in the app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello')
+import './styles.css';
+
+const addTask = document.getElementById('add-task');
+
+addTask.addEventListener('click', () => {
+    const taskModal = document.querySelector('template');
+    const content = taskModal.content.cloneNode(true);
+    document.body.append(content);
+})
