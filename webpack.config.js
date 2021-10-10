b340d7e6a8b3e932c5ea4da3853e20b8dad36935
Miguel Gonzalez <miguelgm.priv@gmail.com>
2021-10-09 18:55:04 -0700

    Add html template to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -10,7 +10,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Task Helper',
+            template: './src/index.html',
+            
         }),
     ],
     output: {
