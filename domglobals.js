10d73f8e7a73525cd025a2501e410e83708c73c7
Miguel Gonzalez <miguelgm.priv@gmail.com>
2021-10-12 16:20:20 -0700

    Remove template for task form and add domglobal.js file Going to fallback to using div as a container after some thought. domglobal.js will keep the variables for accessing the DOM

---
+++ domglobals.js
@@ -0,0 +1,3 @@
+const addTaskContainer = document.getElementById('add-task-container')
+const addTaskButton = document.getElementById('add-task-button');
+const taskForm = document.getElementById('task-form');
